9d023f689cef7df4ee1d09196fa95226a0f4e7df
parkero2 <parkero2@stu.otc.school.nz>
2022-10-02 03:38:39 +1300

    Fixed PHP and database errors. Bundled a PHP.ini file to use

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 $Json = Get-content ./config.json | ConvertFrom-Json
 cd views
-echo $Json.pathtoPHP
+ls
+$inistrng = "-C " + $Json.pathtoPHPconfig
 Start-Process -FilePath $Json.pathtoPHP -ArgumentList "-S localhost:3000"
-cd ..
 #Start-Process cmd.exe -ArgumentList "/c node ."
